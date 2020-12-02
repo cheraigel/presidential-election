fb47c86f6642608670bc3926f711bfe53bcb598a
SahasPunchihewa <sahasmcg2995@gmail.com>
2020-12-02 22:53:27 +0530

    added new candidate add function

--- src/Main/Controller/AdminController.java
+++ src/Main/Controller/AdminController.java
@@ -1,4 +1,35 @@
 package Main.Controller;
 
-public class AdminController {
+import Main.Model.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+
+public class AdminController extends Main
+{
+    @FXML
+    private TextField C_ID,C_Name;
+
+    private String Candidate_ID,Candidate_Name;
+
+    public void candidateadd()
+    {
+        Alert a=new Alert(Alert.AlertType.INFORMATION);
+
+        Candidate_ID=C_ID.getText();
+        Candidate_Name=C_Name.getText();
+
+        try
+        {
+            con.createStatement().execute("insert into candidates('"+Candidate_ID+"','"+Candidate_Name+"')");
+            a.setContentText("Successfully Added !");
+            a.show();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
